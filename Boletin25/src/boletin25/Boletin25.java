e8fa393244104b0e18a32b0f04a0c50e3e1b9844
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2019-04-02 11:47:13 +0200

    Init.

---
+++ Boletin25/src/boletin25/Boletin25.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author finfanterodal
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewUser u1=new NewUser();
+        u1.setVisible(true);
+        
+    }
+    
+}
